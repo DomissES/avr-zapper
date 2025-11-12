c5c1c0ea3216739d47ea5baec090b0c7ff4d0904
DomissES <dominik.otreba96@gmail.com>
2025-11-12 19:35:45 +0100

    Added adc hal

---
+++ source/hal/adc.c
@@ -0,0 +1,219 @@
+/**
+ * @file adc.c
+ * @addtogroup Level_1_HAL
+ *
+ * @brief Source file for adc
+ * TODO:: add more sophisticated description
+ *
+ * @author domis
+ * @date 07.11.2025
+ */
+
+// File specific includes
+#include "adc.h"
+
+#include "global_defines.h"
+#include "gpio.h"
+#include "system_settings.h"
+
+// Target specific includes
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+//===================================================================================================================//
+// Private macro defines                                                                                             //
+//===================================================================================================================//
+
+#define ADC_PRESCALER_2    1
+#define ADC_PRESCALER_4    2
+#define ADC_PRESCALER_8    3
+#define ADC_PRESCALER_16   4
+#define ADC_PRESCALER_32   5
+#define ADC_PRESCALER_64   6
+#define ADC_PRESCALER_128  7
+
+#define ADC_USED_PRESCALER ADC_PRESCALER_128
+
+#define ADC_CHANNEL_1      0
+#define ADC_CHANNEL_2      1
+
+#define ADC_SET_CHANNEL_1  ADCSRA = (ADCSRA & 0xF0) | ADC_CHANNEL_1
+#define ADC_SET_CHANNEL_2  ADCSRA = (ADCSRA & 0xF0) | ADC_CHANNEL_2
+
+// Values referred to proper measuring
+#define ADC_VREF           1300  // mV
+#define ADC_VREF_OFFSET    (-85) // mV
+#define ADC_MAX_RESOLUTION 1023  // mV
+
+// clang-format off
+#define ADC_INIT_STRUCT()                                                                                             \
+ {                                                                                                                    \
+    .channel[0] = &adc_DcdcChannel,                                                                                   \
+    .channel[1] = &adc_ControlChannel,                                                                                \
+    .status = ADC_STATUS_UNITIALIZED,                                                                                 \
+    .activeChannel = eADC_CHANNEL_DC_DC_FB,                                                                           \
+    .checkInputVoltage = true                                                                                         \
+} // clang-format on
+
+//===================================================================================================================//
+// Private definitions                                                                                               //
+//===================================================================================================================//
+
+typedef struct
+{
+    Adc_Channel_t *channel[ADC_CHANNELS];
+    Adc_Status_e   status;
+    Adc_Instance_e activeChannel;
+    bool           checkInputVoltage;
+} Adc_HAL_t;
+
+//===================================================================================================================//
+// Private variables                                                                                                 //
+//===================================================================================================================//
+
+Adc_Channel_t adc_DcdcChannel    = {.channel = eADC_CHANNEL_DC_DC_FB, .lastMeasurement = 0};
+Adc_Channel_t adc_ControlChannel = {.channel = eADC_CHANNEL_CONTROL_IN, .lastMeasurement = 0};
+
+volatile Adc_HAL_t hAdc = ADC_INIT_STRUCT();
+volatile uint16_t  batteryVoltage;
+
+//===================================================================================================================//
+// ISR functions                                                                                                     //
+//===================================================================================================================//
+
+/**
+ * @brief Callback function for adc ISR.
+ *
+ * @param channel channel which the measure was done
+ */
+__weak void Adc_Done_Callback(volatile Adc_Channel_t *channel)
+{
+    (void)channel;
+}
+
+/**
+ * @brief ISR function
+ */
+ISR(ADC_vect)
+{
+    GPIO_OUT_LED_C_ENABLE();
+    hAdc.status = ADC_STATUS_READY;
+
+    if(hAdc.checkInputVoltage)
+    {
+        batteryVoltage = ADC;
+    }
+    else
+    {
+        hAdc.channel[hAdc.activeChannel]->lastMeasurement = ADC;
+        Adc_Done_Callback(hAdc.channel[hAdc.activeChannel]);
+    }
+
+    hAdc.status = ADC_STATUS_IDLE;
+    Adc_Perform();
+
+    GPIO_OUT_LED_C_DISABLE();
+}
+
+//===================================================================================================================//
+// Private functions                                                                                                 //
+//===================================================================================================================//
+
+/**
+ * @brief Jumps to the next available channel for measurement
+ *
+ */
+static void Adc_privJumpToNextChannel()
+{
+
+    if(hAdc.activeChannel == eADC_CHANNEL_CONTROL_IN)
+    {
+        ADMUX = (ADMUX & 0xF0) | eADC_CHANNEL_DC_DC_FB;
+    }
+    else
+    {
+        ADMUX = (ADMUX & 0xF0) | eADC_CHANNEL_CONTROL_IN;
+    }
+}
+
+//===================================================================================================================//
+// Public functions                                                                                                  //
+//===================================================================================================================//
+
+/**
+ * @brief Initializes the ADC with configuration needed for this project
+ *
+ */
+void Adc_Init()
+{
+    // Set voltage reference to internal
+    ADMUX = _BV(REFS0) | _BV(REFS1);
+    // Enable interrupts and prescaler
+    ADCSRA = _BV(ADEN) | _BV(ADIE) | ADC_USED_PRESCALER;
+
+    hAdc.status = ADC_STATUS_IDLE;
+}
+
+/**
+ * @brief Realizes the ADC measurement.
+ *
+ * It selects the next channel and starts the measure. Use this function to enable continuous measuring.
+ *
+ * @return true
+ * @return false
+ */
+bool Adc_Perform()
+{
+    // if measure done, obtain measure
+    // then change to new channel and start again
+
+    // if not, start
+
+    if((hAdc.status != ADC_STATUS_IDLE) || (hAdc.checkInputVoltage == true))
+    {
+        return false;
+    }
+
+    Adc_privJumpToNextChannel();
+
+    ADC_START_MEASURE();
+    hAdc.status = ADC_STATUS_MEASURING;
+
+    return true;
+}
+
+/**
+ * @brief Checks the input voltage by measuring the internal bandgap reference.
+ *
+ * This function is intended to use only once, at system initialization.
+ *
+ * @return battery voltage in milivolts
+ */
+uint16_t Adc_CheckInputVoltage()
+{
+    uint16_t voltage;
+
+    // set proper admux and tweak reference
+    ADMUX = _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    ADMUX = (ADMUX & 0x0F) | (_BV(REFS0)); // Vref to AVcc
+
+    // start measure
+    ADC_START_MEASURE();
+    hAdc.status = ADC_STATUS_MEASURING;
+
+    // wait for it
+    while(hAdc.status != ADC_STATUS_IDLE)
+    {
+        GPIO_OUT_LED_B_ENABLE();
+    }
+
+    // calculate it
+    voltage = (uint16_t)(((uint32_t)(ADC_VREF + ADC_VREF_OFFSET) * ADC_MAX_RESOLUTION) / batteryVoltage); // in mV
+
+    // go back to normal mode
+    // hAdc.checkInputVoltage = false;
+    Adc_Init();
+    GPIO_OUT_LED_B_DISABLE();
+
+    return voltage;
+}
---
+++ source/hal/adc.h
@@ -0,0 +1,102 @@
+/**
+ * @file adc.h
+ * @addtogroup Level_1_HAL
+ *
+ * @brief Header file for adc converter
+ * TODO:: add more sophisticated description and explanation
+ *
+ * @author domis
+ * @date 07.11.2025
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+// File specific includes
+#include "global_defines.h"
+#include "system_settings.h"
+
+// Target specific includes
+
+//===================================================================================================================//
+// Public macro defines                                                                                              //
+//===================================================================================================================//
+
+#define ADC_CHANNELS 2
+
+//===================================================================================================================//
+// Public definitions                                                                                                //
+//===================================================================================================================//
+
+typedef enum
+{
+    eADC_CHANNEL_DC_DC_FB = 0,
+    eADC_CHANNEL_CONTROL_IN
+} Adc_Instance_e;
+
+typedef enum
+{
+    ADC_STATUS_UNITIALIZED,
+    ADC_STATUS_IDLE,
+    ADC_STATUS_MEASURING,
+    ADC_STATUS_READY,
+    ADC_STATUS_ERROR
+} Adc_Status_e;
+
+typedef struct
+{
+    Adc_Instance_e channel;
+    uint16_t       lastMeasurement;
+} Adc_Channel_t;
+
+//===================================================================================================================//
+// Public variables                                                                                                  //
+//===================================================================================================================//
+
+//===================================================================================================================//
+// Public functions                                                                                                  //
+//===================================================================================================================//
+
+/**
+ * @brief Starts measure process for ADC
+ */
+#define ADC_START_MEASURE() ADCSRA |= _BV(ADSC)
+
+/**
+ * @brief Stops measure process for ADC
+ */
+#define ADC_STOP_MEASURE()  ADCSRA &= ~_BV(ADSC)
+
+/**
+ * @brief Callback function for adc ISR.
+ *
+ * @param channel channel which the measure was done
+ */
+void Adc_Done_Callback();
+
+/**
+ * @brief Initializes the ADC with configuration needed for this project
+ *
+ */
+void Adc_Init();
+
+/**
+ * @brief Realizes the ADC measurement.
+ *
+ * It selects the next channel and starts the measure. Use this function to enable continuous measuring.
+ *
+ * @return true
+ * @return false
+ */
+bool Adc_Perform();
+
+/**
+ * @brief Checks the input voltage by measuring the internal bandgap reference.
+ *
+ * This function is intended to use only once, at system initialization.
+ *
+ * @return battery voltage in milivolts
+ */
+uint16_t Adc_CheckInputVoltage();
+
+#endif // ADC_H_
